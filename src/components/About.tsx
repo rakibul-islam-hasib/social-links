9f529bd1344c1b8e127d626ba50b1d792088056f
Hasib Hossain <boraborhasib@gmail.com>
2023-10-14 21:32:10 +0600

    More done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,11 @@ const About: React.FC = () => {
     return (
         <div className='grid grid-cols-2'>
             <div className="flex gap-2 ">
-                <img src={myImg} className='h-[250px] w-[250px]' alt="My Images " />
+                <img src={myImg} className='h-[150px] w-[150px]' alt="My Images " />
                 <div className="">
-                    <h1 className='text-2xl font-serif'>Md Rakibul Islam</h1>
+                    <h1 className='text-3xl font-bebas-neue'>Md Rakibul Islam</h1>
+                    <p className='font-barlow-semi-condensed font-medium leading-3'>Web Developer</p>
+                    <p className='font-barlow-semi-condensed font-light leading-5 mt-3'>I am a web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
                 </div>
             </div>
             <div className="">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bebas-neue': ['Bebas Neue', 'sans-serif'],
+        'barlow-semi-condensed': ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
