cd6e80516ff2222da7a8a91a16771c4d02d59725
Hasib Hossain <boraborhasib@gmail.com>
2023-10-14 22:15:54 +0600

    More done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import myImg from '../assets/img/me.png';
+import { AiOutlineLink } from 'react-icons/ai';
 const About: React.FC = () => {
     return (
         <div className='grid grid-cols-2'>
@@ -9,6 +10,14 @@ const About: React.FC = () => {
                     <h1 className='text-3xl font-bebas-neue'>Md Rakibul Islam</h1>
                     <p className='font-barlow-semi-condensed font-medium leading-3'>Web Developer</p>
                     <p className='font-barlow-semi-condensed font-light leading-5 mt-3'>I am a web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
+                    <p>
+                        <AiOutlineLink className='inline-block' />
+                        <a href="https://rakibul.tech" target='_blank' className='hover:text-blue-600 visited:text-blue-700 underline'>rakibul.tech</a>
+                    </p>
+                    <p>
+                        <AiOutlineLink className='inline-block' />
+                        <a href="https://social.rakibul.tech" target='_blank' className='hover:text-blue-600 visited:text-blue-700 underline'>social.rakibul.tech</a>
+                    </p>
                 </div>
             </div>
             <div className="">
---
+++ src/components/Accounts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accounts: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Accounts;
